335fdfc996ed5765c2705111585d69771cf18ca9
Natalya <gruzdevanatalya5@gmail.com>
2022-07-12 19:19:12 +0300

    cart + webpack

---
+++ js/cart.js
@@ -0,0 +1,14 @@
+const cartBtn = document.querySelector('#cart');
+const cartModal = document.querySelector('.cart');
+const cartCloseBtn = document.querySelector('.cart-close');
+
+const openCart = () => {
+    cartModal.style.display = 'flex'
+}
+
+const closeCart = () => {
+    cartModal.style.display = ''
+}
+
+cartBtn.addEventListener('click', openCart);
+cartCloseBtn.addEventListener('click', closeCart);
