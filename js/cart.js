6b239ccef56e6d675d600c940b445c9506ac99e0
Natalya <gruzdevanatalya5@gmail.com>
2022-07-12 19:29:29 +0300

    gitignore

--- js/cart.js
+++ js/cart.js
@@ -1,14 +1,18 @@
-const cartBtn = document.querySelector('#cart');
-const cartModal = document.querySelector('.cart');
-const cartCloseBtn = document.querySelector('.cart-close');
+const cart = () => {
+    const cartBtn = document.querySelector('#cart');
+    const cartModal = document.querySelector('.cart');
+    const cartCloseBtn = document.querySelector('.cart-close');
 
-const openCart = () => {
-    cartModal.style.display = 'flex'
+    const openCart = () => {
+        cartModal.style.display = 'flex';
+    }
+
+    const closeCart = () => {
+        cartModal.style.display = '';
+    }
+
+    cartBtn.addEventListener('click', openCart);
+    cartCloseBtn.addEventListener('click', closeCart);
 }
 
-const closeCart = () => {
-    cartModal.style.display = ''
-}
-
-cartBtn.addEventListener('click', openCart);
-cartCloseBtn.addEventListener('click', closeCart);
+cart();
